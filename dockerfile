95f171844aa281f806c5417d228c4e008df3222a
Khai Hoan Pham <khaihoanqn@gmail.com>
2021-08-10 11:03:45 +0700

    Basically Dockerize the webapp, update README.md

--- dockerfile
+++ dockerfile
@@ -1,6 +1,5 @@
 FROM python:3.8
 WORKDIR /app
-COPY requirements.txt requirements.txt
+COPY requirements.txt /app/
 RUN pip3 install -r requirements.txt
-COPY . .
-CMD ['python3' 'manage.py' 'runserver']
+COPY . /app/
--- project4/settings.py
+++ project4/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '13kl@xtukpwe&xj2xoysxe9_6=tf@f8ewxer5n&ifnd46+6$%8'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 INTERNAL_IPS = [
     '127.0.0.1',
@@ -93,12 +93,20 @@ DATABASES = {
     #     'ENGINE': 'django.db.backends.sqlite3',
     #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     # },
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'NAME': 'network_db',
+    #     'USER': 'postgres',
+    #     'PASSWORD': 'postgres',
+    #     'HOST': '127.0.0.1',
+    #     'PORT': '5432',
+    # },
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'network_db',
+        'NAME': 'postgres',
         'USER': 'postgres',
         'PASSWORD': 'postgres',
-        'HOST': '127.0.0.1',
+        'HOST': 'db',
         'PORT': '5432',
     }
 }
